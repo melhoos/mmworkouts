0b1b0e84b0b3ac5fdea3737cc68b6101cd7fe0ab
melhoos <mmelhoos@gmail.com>
2021-11-14 11:31:11 +0100

    Updated model

--- src/models/exercise.model.ts
+++ src/models/exercise.model.ts
@@ -4,12 +4,4 @@ export interface Exercise {
   type: string;
   main_muscle_group: string[];
   equipments: string[];
-  // name_no: string;
-  // name_en: string;
-  // exercise_type_id: string;
-  // main_muscle_group_ids: string[];
-  // equipments_needed_ids: string[];
-  // consideration_ids: string[];
-  // tips: string;
-  // url: string;
 }
